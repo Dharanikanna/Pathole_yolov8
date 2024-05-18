cbbef39c26e6f2b80b536bfdaf658c72f4edcb91
uif02672 <dharani.kanna.k.v@continental-corporation.com>
2024-05-19 00:49:27 +0530

    optimization begin

--- train.py
+++ train.py
@@ -2,10 +2,16 @@ from ultralytics import YOLO
 import torch
 
 #model load
-model = YOLO("yolov8n.yaml")
+model = YOLO("architecture/yolov8n.yaml")    # train in nano model
+# model = YOLO("architecture/yolov8s.yaml")    # train in small model
+# model = YOLO("architecture/yolov8m.yaml")    # train in medium model
+# model = YOLO("architecture/yolov8l.yaml")    # train in large model
+# model = YOLO("architecture/yolov8x.yaml")    # train in xlarge model
+
+
 
 # Start training
-model.train(data="config.yaml", epochs=1)
+model.train(data="config.yaml", epochs=1, batch=3)
 
 # Save trained model
 #model.save("Path_hole.pt")
